af791801245bb8efec6df197c5cf2703efb6b2b9
Shaheen Ghiassy <s.ghiassy@gmail.com>
2016-02-25 10:54:02 -0800

    Automated moving good to production and backing up bad

--- scripts/badtogood.sh
+++ scripts/badtogood.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mv ./../public/javascripts/dealdetails.jsbundle ./../public/javascripts/dealdetails.bad.jsbundle
-mv ./../public/javascripts/dealdetails.good.jsbundle ./../public/javascripts/dealdetails.jsbundle
+mv ../public/javascripts/dealdetails.jsbundle ../public/javascripts/dealdetails.bad.jsbundle
+mv ../public/javascripts/dealdetails.good.jsbundle ../public/javascripts/dealdetails.jsbundle
 
 git add .;
 git commit -m "Automated moving good to production and backing up bad";
